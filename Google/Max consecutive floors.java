67e0511487078b040bd3272dcfe162292b1490e0
VINISHAVEERA <121846571+VINISHAVEERA@users.noreply.github.com>
2023-02-20 12:17:07 +0530

    Update Max consecutive floors.java

--- Google/Max consecutive floors.java
+++ Google/Max consecutive floors.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxConsecutive(int bottom, int top, int[] special) {
     int max= Integer.MIN_VALUE;
     Arrays.sort(special);
-    max = Math.max(max,special[0]-bottom);
+    max = Math.max(max,special[0]-bottom)
 
     for(int i=1;i<special.length;i++){
         max=Math.max(max,special[i]-special[i-1]-1);
